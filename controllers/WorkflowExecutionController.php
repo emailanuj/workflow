fe5ac89d340a29cae103f125ea6fc6ccf60d18a0
lavijaindev <lavijain91@gmail.com>
2020-05-05 10:45:55 +0530

    graph render for execution

--- controllers/WorkflowExecutionController.php
+++ controllers/WorkflowExecutionController.php
@@ -30,31 +30,31 @@ class WorkflowExecutionController extends Controller
         ];
     }
 
-    public function actionIndex() {        
-        $id = Yii::$app->request->get('id');            
-            $model = new Workflow();
-            if (($workflow_data = Workflow::findOne($id)) !== null) {
-              $workflowjson = json_decode($workflow_data->workflow_data);
-              foreach($workflowjson as $wjk =>$wjv) {
-                  //(
-//     [step_no] => 12
-//     [if_fail] => stop
-//     [next_process] => 1
-//     [keywords] => NSO
-//     [api_url] => 
-//     [api_method] => 
-//     [api_type] => 
-//     [api_headers] => 
-//     [function_execute] => 
-//     [auth_type] => 
-//     [token_from] => 
-//     [token_url] => 
-//     [username] => 
-//     [password] => 
-//     [data_source] => form_data
-//     [get_data_function] => 
-//     [form_data] => form data
-// )
+    public function actionIndex() { 
+        $this->layout = 'workflowLayout';               
+        $id = Yii::$app->request->get('id');        
+        if (($model = Workflow::findOne($id)) !== null) {
+            $workflowjson = json_decode($model->workflow_data);
+            foreach($workflowjson as $wjk =>$wjv) {
+                                        //(
+                                        //     [step_no] => 12
+                                        //     [if_fail] => stop
+                                        //     [next_process] => 1
+                                        //     [keywords] => NSO
+                                        //     [api_url] => 
+                                        //     [api_method] => 
+                                        //     [api_type] => 
+                                        //     [api_headers] => 
+                                        //     [function_execute] => 
+                                        //     [auth_type] => 
+                                        //     [token_from] => 
+                                        //     [token_url] => 
+                                        //     [username] => 
+                                        //     [password] => 
+                                        //     [data_source] => form_data
+                                        //     [get_data_function] => 
+                                        //     [form_data] => form data
+                                        // )
                 switch ($wjv->keywords) {
                     case "API":
                         $result = "API";
@@ -69,11 +69,12 @@ class WorkflowExecutionController extends Controller
                     default:
                         $result = "Default";
                 }
-              }  
-                return $this->render('index', [                    
-                    'result'    => $result,
-                ]);                          
-            }                    
+            }  
+            return $this->render('index', [                    
+                'model'    => $model,                
+                'workflow_id' => $id,
+            ]);                          
+        }                    
                       
     }
     
