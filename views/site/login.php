f0b74888c334c4438a0da6ead44560319236d52e
= <email.anujdubey@gmail.com>
2020-05-06 17:04:31 +0530

    Login Design Changed

--- views/site/login.php
+++ views/site/login.php
@@ -10,22 +10,24 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login text-center" style="width: 400px;">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal'
-    ]); ?>
-
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
+<div class="container" style="margin-left:-150px;" >
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4">
+					<div class="login-box" style="width: auto; padding: 5px 15px 15px; border: 1px solid #bbb; border-radius: 10px;">
+						<div class="row">
+							<div class="col-md-12">
+								<h3 class="text-center"><?= Html::encode($this->title) ?></h3>
+								<p class="text-center">Please fill following fields</p>
+								<?php $form = ActiveForm::begin([
+                                    'id' => 'login-form',
+                                    'layout' => 'horizontal'
+                                ]); ?>
+                            
+                                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                            
+                                    <?= $form->field($model, 'password')->passwordInput() ?>
+                            
+                                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
@@ -33,4 +35,10 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
 
     <?php ActiveForm::end(); ?>
-</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
