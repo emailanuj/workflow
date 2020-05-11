828bb772a4b191ae1cbe6690515a6171ceae98db
lavijaindev <lavijain91@gmail.com>
2020-05-11 11:21:33 +0530

    changes for execution id

--- controllers/WorkflowExecutionController.php
+++ controllers/WorkflowExecutionController.php
@@ -34,8 +34,10 @@ class WorkflowExecutionController extends Controller
         $this->layout = 'workflowLayout';               
         $id = Yii::$app->request->get('id');               
         if (($model = Workflow::findOne($id)) !== null) {
-            $workflowjson = json_decode($model->workflow_data);            
-            foreach($workflowjson as $wjk =>$wjv) {                
+            $workflowjson = json_decode($model->workflow_data); 
+            $workflow_elements = count((array)$workflowjson);        
+            $execution_id = uniqid('ex');
+            foreach($workflowjson as $wjk =>$wjv) {                                
                 switch ($wjv->keywords) {
                     case "API":
                         $result = "API";
@@ -43,6 +45,8 @@ class WorkflowExecutionController extends Controller
                     case "NSO":
                         if($wjv->data_source == 'form_data') {
                             $result = $wjv->form_data;
+                        } else if($wjv->data_source == 'function_name') {
+                            $result = $wjv->get_data_function;                            
                         } else {
                             $result = '';
                         }                        
@@ -53,18 +57,23 @@ class WorkflowExecutionController extends Controller
                     default:
                         $result = "Default";
                 }
-                $ex_model_saved = WorkflowExecution::find()->where(['instance_id' => $model->id])->one();
-                if($ex_model_saved->instance_id == '') {
+                $ex_model_saved = WorkflowExecution::find()->where(['instance_id' => $model->id, 'request_params' => $wjv->step_no, 'execution_id' => $execution_id])->one();                
+                if(empty($ex_model_saved)) {                    
                     $ex_model = new WorkflowExecution(); 
                     $ex_model->instance_id      = $model->id;
-                    $ex_model->request_params   = $wjv->keywords;
-                    $ex_model->response_params  = $result;                
+                    $ex_model->request_params   = $wjv->step_no;
+                    $ex_model->response_params  = $result;
+                    $ex_model->execution_id     = $execution_id;  
+                    $ex_model->status           = '1';              
                     $ex_model->save();                
                 }                
-            }  
+            } 
+            // get executed data  
+            $execution_model = WorkflowExecution::find()->where(['instance_id' => $model->id, 'execution_id' => $execution_id])->all();            
+
             return $this->render('index', [                    
                 'model'    => $model, 
-                //'ex_model' => $ex_model,               
+                'execution_model' => $execution_model,               
                 'workflow_id' => $id,
             ]);                          
         }                    
--- views/workflow-execution/index.php
+++ views/workflow-execution/index.php
@@ -1,15 +1,13 @@
 <?php
 use yii\helpers\Url;
+
 ?>
 <div>    
     <!-- <div id="mySvg"></div> -->
     <div id="mySvg" style="position: absolute; border-width: 1px; overflow: hidden; width: 100%; height: 100%; border-color: rgb(255, 255, 255); border-style: solid; background-color: rgb(255, 255, 255); background-image: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI0MCIgaGVpZ2h0PSI0MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAwIDEwIEwgNDAgMTAgTSAxMCAwIEwgMTAgNDAgTSAwIDIwIEwgNDAgMjAgTSAyMCAwIEwgMjAgNDAgTSAwIDMwIEwgNDAgMzAgTSAzMCAwIEwgMzAgNDAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI2QwZDBkMCIgb3BhY2l0eT0iMC4yIiBzdHJva2Utd2lkdGg9IjEiLz48cGF0aCBkPSJNIDQwIDAgTCAwIDAgMCA0MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZDBkMGQwIiBzdHJva2Utd2lkdGg9IjEiLz48L3BhdHRlcm4+PC9kZWZzPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JpZCkiLz48L3N2Zz4='); background-position: -1px -1px;"></div>
     
-    
-    
 	<input type="hidden" name="workflow_id" id="workflow_id" value="<?php echo Yii::$app->request->get('id');?>">
-    <div class="content-wrapper content-wrapper-app">
-      
+    <div class="content-wrapper content-wrapper-app">      
       <div class="svg-wrapper">
         <div id="mySvg"><svg width="1536" height="754"></svg></div>
       </div>
