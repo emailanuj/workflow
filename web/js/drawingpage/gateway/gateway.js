cfa65adb5ccf5d56e9e218b7535ed6c6c1b0a916
lavijaindev <lavijain91@gmail.com>
2020-05-30 16:30:31 +0530

    bpmn js library changes

--- web/js/drawingpage/gateway/gateway.js
+++ web/js/drawingpage/gateway/gateway.js
@@ -1,43 +1,32 @@
 "use strict";
-
+var width = 120, height = 80, dragbarw = 20;
 var gatewaydevider = function (eid,subElement,svg,xvalue,yvalue){
-
 var g2 = svg.append('g')
         .attr('transform', 'translate(' + xvalue + ',' + yvalue + ')')
         .attr('id', 'gateway' + (++idgatewayelement))
         .call(drag);
-
-        //.attr("transform", "matrix(0.7,0.7,-0.7,0.7," + d3.event.pageX + "," + d3.event.pageY + ")")
-
         if (subElement === "exclusive") {
-
             g2.append('path')
             .attr("d","m 12.8,12 5.942857142857143,7.771428571428571 -5.942857142857143,7.771428571428571,2.742857142857143,0 4.571428571428571,-5.971382857142857 4.571428571428571,5.971382857142857,2.742857142857143,0 -5.942857142857143,-7.771428571428571 5.942857142857143,-7.771428571428571,-2.742857142857143,0 -4.571428571428571,5.971382857142857 -4.571428571428571,-5.971382857142857,-2.742857142857143,0 z")   
             .attr("transform","matrix(1.4375,0,0,1.4375,-28.9375,1.9375)")
             //.call(drag);
-
            }else if (subElement === "parallel") {
-
                 g2.append('path')
                 .attr("d","M11.25,20.5L30.25,20.5M20.5,11.25L20.5,30.25")
                 .attr("stroke-width", "4")
                 .attr("fill", "none")
                 .attr("stroke", "#808080")   
                 .attr("transform","matrix(1,0,0,1,-21,9)")
-
            }else if (subElement === "inclusive") {
-
               g2.append('circle')
                 .attr("r","12.428571428571429") 
                 .attr("stroke-width", "4")
                 .attr("fill", "none")
                 .attr("stroke", "#808080")   
                 .attr("cx", "0")
-                .attr("cy", "30")
-        // .attr("transform","matrix(1,0,0,1,-21,9)")
+                .attr("cy", "30")        
 
            }else if (subElement === "event") {
-
                 g2.append('circle')
                     .attr("r","14.428571428571429")
                     .attr("stroke-width", "1")
@@ -45,22 +34,29 @@ var g2 = svg.append('g')
                     .attr("stroke", "#808080")
                     .attr("cx", "0")
                     .attr("cy", "30");
-
                 g2.append('circle')
-                .attr("r","12.428571428571429")
-                .attr("stroke-width", "1")
-                .attr("fill", "none")
-                .attr("stroke", "#808080")
-                .attr("cx", "0")
-                .attr("cy", "30");
-
+                    .attr("r","12.428571428571429")
+                    .attr("stroke-width", "1")
+                    .attr("fill", "none")
+                    .attr("stroke", "#808080")
+                    .attr("cx", "0")
+                    .attr("cy", "30");
                 g2.append('path')
-                .attr("d","M24.827514,26.844972L15.759248000000001,26.844216L12.957720300000002,18.219549L20.294545,12.889969L27.630481000000003,18.220774L24.827514,26.844972Z")   
-                .attr("stroke-width", "1.5")
-                .attr("fill", "none")
-                .attr("stroke", "#808080") 
-                .attr("transform","matrix(1,0,0,1,-20,9)")
+                    .attr("d","M24.827514,26.844972L15.759248000000001,26.844216L12.957720300000002,18.219549L20.294545,12.889969L27.630481000000003,18.220774L24.827514,26.844972Z")   
+                    .attr("stroke-width", "1.5")
+                    .attr("fill", "none")
+                    .attr("stroke", "#808080") 
+                    .attr("transform","matrix(1,0,0,1,-20,9)")
             }
+
+            g2.append('foreignObject')
+                .attr('id', 'fobject' + idgatewayelement)
+                .attr("x", function(d) { return 0 ; })
+                .attr("y", function(d) { return 0; })
+                .attr('width', width - 60)
+                .attr('height', height - 40)
+                .html("<div id=\"textidgateway"+idgatewayelement+"\"; style=\"width: 80%; height:45px ; background-color: transparent;\" >Gateway</div>");
+
 
             g2.append('rect')
                 .attr('id', 'gateway' + idgatewayelement)
@@ -105,7 +101,7 @@ var g2 = svg.append('g')
                     .duration(200)
                     .style("opacity", 1.9);
 
-                tooltipDiv.html("<input id=" + "trash-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/trash-icon.png" + " alt=" + "trash" + " style=" + "width:25px;" + " >"+"&nbsp"+"<input id=" + "arrow-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/arrow.png" + " alt=" + "arrow" + " style=" + "width:25px;" + " >" + "<br>" + "<input id=" + "property-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/settingsicon.png" + " alt=" + "trash" + " style=" + "width:25px;" + " >")
+                tooltipDiv.html("<input id=" + "trash-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/trash-icon.png" + " alt=" + "trash" + " style=" + "width:25px;" + " >"+"&nbsp"+"<input id=" + "arrow-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/arrow.png" + " alt=" + "arrow" + " style=" + "width:25px;" + " >" + "<br>" + "<input id=" + "property-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/settingsicon.png" + " alt=" + "trash" + " style=" + "width:25px;" + " >"+"<input id=" + "text-button" + " type=" + "image" + " title=" + "End Event" + " src=" + baseURL + "/img/review.png" + " alt=" + "Text" + " style=" + "width:25px;" + " >")
                     .style("left", coords.x + 45 + "px")
                     .style("top", (coords.y + 20) + "px");
 
@@ -143,7 +139,18 @@ var g2 = svg.append('g')
                    tooltipDiv.style("opacity", 0);
                    console.log("gateway evnt button clicked ")
                 });
-
+                tooltipDiv.select("#text-button").on("click", function () {
+                    tooltipDiv.style("opacity", 0);
+                    console.log("gateway button clicked ")
+                    var element = document.getElementById('edittext');
+                    element.style.width = width+"px";
+                    element.style.height = height+"px";
+                    element.style.display = "block";
+                    element.style.left = coords.x+"px";
+                    element.style.top = coords.y+"px";                    
+                    element.value = document.getElementById("textid"+t).innerHTML;
+                    window.selectedtextid = "textid"+t;                    
+                });
                 tooltipDiv.select("#arrow-button").on("click", function () {
                     tooltipDiv.style("opacity", 0);
                     console.log("end arrow button clicked ");
@@ -221,22 +228,14 @@ var g2 = svg.append('g')
                         endx = coords.x + 35;
                         endy = coords.y + 30;
                     }
-                    
                     endid =t;
                     midx = startx + ((endx - startx) / 2);
-                    flowcreator();
-
-                  
+                    flowcreator();  
                 }
             })
-         //   .call(drags)
-
-
             if (eid === null) {
                 eid ='gateway'+idgatewayelement;
             }
-
               EventBPMNJsonCreator(eid, xvalue, yvalue, 120, 80,"gateway",subElement);
               subElement = null;
-
  }
