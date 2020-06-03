8df02cf5c000140b5918b3c4ddfe6914e96d68c3
Kuldeep Pandey -X (kulpande) <kulpande@cisco.com>
2020-06-03 15:13:31 +0530

    code formate

--- web/js/drawingpage/flow/flow.js
+++ web/js/drawingpage/flow/flow.js
@@ -1,124 +1,110 @@
 "use strict";
-var flowcreator = function (eid,subElement,svg,xvalue,yvalue){
-    console.log(eid+'---'+svg+'----'+xvalue+'-----'+yvalue);
-    // var group = svg.append('g')
-    //     .attr('transform', 'translate(' + xvalue + ',' + yvalue + ')')
-    //     .attr('id', 'flow' + (++idflow))
-    //     .call(drag) 
-    // var groupSelect = svg.append('g')
-    if(midx == 0 ||  endx == 0 || endy == 0 || startx == 0 || starty == 0) {
+var flowcreator = function (eid, subElement, svg, xvalue, yvalue) {
+    console.log(eid + '---' + svg + '----' + xvalue + '-----' + yvalue);
+    if (midx == 0 || endx == 0 || endy == 0 || startx == 0 || starty == 0) {
         return false;
-    } 
+    }
     if (eid === null || eid == undefined) {
-        eid = 'flow'+(++idflow);
+        eid = 'flow' + (++idflow);
     }
     var dragger = d3.behavior.drag() //flow drag
-            .on('drag', handleDrag)
-            .on('dragend', function(d){
-                dragging = false;
-            });
-    var sampleSVGflow = sampleSVG.append('g')    
-                        .attr("id", "group"+eid);
-    
-        sampleSVG.append("marker")
-                        .attr("id", "triangle"+idflow)
-                        .attr("viewBox", "0 0 10 10")
-                        .attr("refX", "0")
-                        .attr("refY", "5")
-                        .attr("markerUnits", "strokeWidth")
-                        .attr("markerWidth", "5")
-                        .attr("markerHeight", "4")
-                        .attr("orient", "auto")
-                        .append('svg:path')
-                        .attr('d', 'M 0 0 L 10 5 L 0 10 z');
-        
-    
-        
-        // var strPoints = 'M'+startx + ','+ starty;
-        // if( endx >  startx ){
-        //     if( endy >  starty ){
-        //         strPoints += ' L'+ endx +','+ starty;
-        //     } else if( endy <  starty ){
-        //         strPoints += ' L'+ endx +','+ starty;
-        //     }
-        // } else if( endx <  startx ){
-        //     if( endy >  starty ){
-        //         strPoints += ' L'+ endx +','+ starty;
-        //     } else if( endy <  starty ){
-        //         strPoints += ' L'+ endx +','+ starty;
-        //     }
-        // }
-        // strPoints += ' L'+ endx +','+ endy;
-        
+        .on('drag', handleDrag)
+        .on('dragend', function (d) {
+            dragging = false;
+        });
+    var sampleSVGflow = sampleSVG.append('g')
+        .attr("id", "group" + eid);
 
-        /*
-            Case 1 : ( end.x > start.x ) && ( end.y > start.y )
-            Case 2 : ( end.x > start.x ) && ( end.y < start.y )
+    sampleSVG.append("marker")
+        .attr("id", "triangle" + idflow)
+        .attr("viewBox", "0 0 10 10")
+        .attr("refX", "0")
+        .attr("refY", "5")
+        .attr("markerUnits", "strokeWidth")
+        .attr("markerWidth", "5")
+        .attr("markerHeight", "4")
+        .attr("orient", "auto")
+        .append('svg:path')
+        .attr('d', 'M 0 0 L 10 5 L 0 10 z');
 
-            Case 3 : ( end.x < start.x ) && ( end.y > start.y )
-            Case 4 : ( end.x < start.x ) && ( end.y < start.y )
-        */
-        
-       var strPoints = 'M'+startx + ','+ starty;
-        
-       if( endx >  startx ){
-           var newMidPts = (startx + ((endx - startx)/2) );
-           // console.log('NOT TO GO..'+ coordsY +' @@@@ '+ starty +' @@@ '+ (starty + 60 ));
-           if( coordsY < (starty + 60 ) ){
-               strPoints += ' L'+ newMidPts +','+ starty;
-               strPoints += ' L'+ newMidPts +','+ endy;
-           } else {
-               strPoints += ' L'+ endx +','+ starty;
-           }
-       } else if( endx <  startx ){
-           var newMidPts = (startx - (( startx - endx)/2) );
-           if( coordsY < (starty + 60 ) ){
-               strPoints += ' L'+ newMidPts +','+ starty;
-               strPoints += ' L'+ newMidPts +','+ endy;
-           } else {
-               strPoints += ' L'+ endx +','+ starty;
-           }
-       }
+    // var strPoints = 'M'+startx + ','+ starty;
+    // if( endx >  startx ){
+    //     if( endy >  starty ){
+    //         strPoints += ' L'+ endx +','+ starty;
+    //     } else if( endy <  starty ){
+    //         strPoints += ' L'+ endx +','+ starty;
+    //     }
+    // } else if( endx <  startx ){
+    //     if( endy >  starty ){
+    //         strPoints += ' L'+ endx +','+ starty;
+    //     } else if( endy <  starty ){
+    //         strPoints += ' L'+ endx +','+ starty;
+    //     }
+    // }
+    // strPoints += ' L'+ endx +','+ endy;
 
-       strPoints += ' L'+ endx +','+ endy;
-       
-        console.log( 'PRINCE  :  '+strPoints);
-    
-        var pathPoints = strPoints;
 
-        // group.append("polyline")
-        //     .attr("stroke", "black")
-        //     .attr("points", pathPoints)
-        //     .attr("fill", 'none')
-        //     .attr("stroke-width", "2")
+    /*
+        Case 1 : ( end.x > start.x ) && ( end.y > start.y )
+        Case 2 : ( end.x > start.x ) && ( end.y < start.y )
 
-        sampleSVGflow.append('path')
-                    .attr("id", eid)
-                    .attr("marker-end", "url(#triangle"+idflow+")")
-                    .attr("d", pathPoints )
-                    .attr("stroke", "black")
-                    .attr("stroke-width", 2)
-                    .attr("fill", "none")
-                
-                    
-                    if(startx > 0 && endx > 0){
-                        FlowBPMNJsonCreator(eid,"flow", startid, endid, startx, starty,endx,endy,midx,starttype,endtype); 
-                    }
-                    starttype= "";
-                    endtype = "";
-                    startx = 0;
-                    starty = 0;
-                    endx = 0;
-                    endy = 0;
-                    startid =0;
-                    endid =0;
-                    midx =0;
-                    points.splice(0);
-                    drawing = false;
- }
- function handleDrag() {
-     console.log('dragging');
-    if(drawing) return;
+        Case 3 : ( end.x < start.x ) && ( end.y > start.y )
+        Case 4 : ( end.x < start.x ) && ( end.y < start.y )
+    */
+
+    var strPoints = 'M' + startx + ',' + starty;
+
+    if (endx > startx) {
+        var newMidPts = (startx + ((endx - startx) / 2));
+        // console.log('NOT TO GO..'+ coordsY +' @@@@ '+ starty +' @@@ '+ (starty + 60 ));
+        if (coordsY < (starty + 60)) {
+            strPoints += ' L' + newMidPts + ',' + starty;
+            strPoints += ' L' + newMidPts + ',' + endy;
+        } else {
+            strPoints += ' L' + endx + ',' + starty;
+        }
+    } else if (endx < startx) {
+        var newMidPts = (startx - ((startx - endx) / 2));
+        if (coordsY < (starty + 60)) {
+            strPoints += ' L' + newMidPts + ',' + starty;
+            strPoints += ' L' + newMidPts + ',' + endy;
+        } else {
+            strPoints += ' L' + endx + ',' + starty;
+        }
+    }
+
+    strPoints += ' L' + endx + ',' + endy;
+
+    console.log('PRINCE  :  ' + strPoints);
+
+    var pathPoints = strPoints;
+    sampleSVGflow.append('path')
+        .attr("id", eid)
+        .attr("marker-end", "url(#triangle" + idflow + ")")
+        .attr("d", pathPoints)
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
+
+
+    if (startx > 0 && endx > 0) {
+        FlowBPMNJsonCreator(eid, "flow", startid, endid, startx, starty, endx, endy, midx, starttype, endtype);
+    }
+    starttype = "";
+    endtype = "";
+    startx = 0;
+    starty = 0;
+    endx = 0;
+    endy = 0;
+    startid = 0;
+    endid = 0;
+    midx = 0;
+    points.splice(0);
+    drawing = false;
+}
+function handleDrag() {
+    console.log('dragging');
+    if (drawing) return;
     var dragCircle = d3.select(this), newPoints = [], circle;
     dragging = true;
     // console.log(this.parentNode);
@@ -126,8 +112,8 @@ var flowcreator = function (eid,subElement,svg,xvalue,yvalue){
     var circles = d3.select(this.parentNode).selectAll('circle');
     // console.log('circle length : '+circles[0].length);
     dragCircle
-    .attr('cx', d3.event.x)
-    .attr('cy', d3.event.y);
+        .attr('cx', d3.event.x)
+        .attr('cy', d3.event.y);
     for (var i = 0; i < circles[0].length; i++) {
         circle = d3.select(circles[0][i]);
         newPoints.push([circle.attr('cx'), circle.attr('cy')]);
