8f1237e5306d96feac931a827726783cc3f50f81
Kuldeep Pandey -X (kulpande) <kulpande@cisco.com>
2020-06-18 16:19:44 +0530

    added new theme

--- modules/bpaService/views/bandwidth-circuit-service/index.php
+++ modules/bpaService/views/bandwidth-circuit-service/index.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 $this->registerJsFile('@web/js/bpa-service.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 ?>
 <div class="bpa-usecase-circuit-index">
-    <h3><?= Html::encode($this->title) ?></h3>
+    <!-- <h3><?= Html::encode($this->title) ?></h3> -->
     <?php
 
     $form = ActiveForm::begin([
@@ -24,36 +24,38 @@ $this->registerJsFile('@web/js/bpa-service.js', ['depends' => [\yii\web\JqueryAs
         'enableClientValidation' => true,
     ]);
     ?>
+    <div class="form-group row">
+        <div class="col-md-4">
+            <?= $form->field($bandwidthCircuitServiceModel, 'utilization')->dropDownList($utilization, ['prompt' => 'Please Select']) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($bandwidthCircuitServiceModel, 'duration')->dropDownList($duration, ['prompt' => 'Please Select']) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($bandwidthCircuitServiceModel, 'utilization_type')->dropDownList($utilizationType, ['prompt' => 'Please Select']) ?>
+        </div>
+        <div class="row" id="multicircuit">
+            <div class="col-md-3">
+                <?= $form->field($bandwidthCircuitServiceModel, 'a_end_host[]')->textInput([]) ?>
+            </div>
+            <div class="col-md-3">
+                <?= $form->field($bandwidthCircuitServiceModel, 'a_end_ip[]')->textInput([]) ?>
+            </div>
+            <div class="col-md-3">
+                <?= $form->field($bandwidthCircuitServiceModel, 'z_end_host[]')->textInput([]) ?>
+            </div>
+            <div class="col-md-3">
+                <?= $form->field($bandwidthCircuitServiceModel, 'z_end_ip[]')->textInput([]) ?>
+            </div>
+        </div>
+        <div class="col-md-6"></div>
+        <div class="col-md-2"><a id="addfields" style="margin-top:10px;cursor:pointer;">Add Circuit</a></div>
+        <div class="col-md-2">
 
-    <div class="form-group col-md-4">
-        <?= $form->field($bandwidthCircuitServiceModel, 'utilization')->dropDownList($utilization, ['prompt' => 'Please Select']) ?>
-    </div>
-    <div class="form-group col-md-4">
-        <?= $form->field($bandwidthCircuitServiceModel, 'duration')->dropDownList($duration, ['prompt' => 'Please Select']) ?>
-    </div>
-    <div class="form-group col-md-4">
-        <?= $form->field($bandwidthCircuitServiceModel, 'utilization_type')->dropDownList($utilizationType, ['prompt' => 'Please Select']) ?>
-    </div>
-    <div id="multicircuit">
-        <div class="form-group col-md-3">
-            <?= $form->field($bandwidthCircuitServiceModel, 'a_end_host[]')->textInput([]) ?>
-        </div>
-        <div class="form-group col-md-3">
-            <?= $form->field($bandwidthCircuitServiceModel, 'a_end_ip[]')->textInput([]) ?>
-        </div>
-        <div class="form-group col-md-3">
-            <?= $form->field($bandwidthCircuitServiceModel, 'z_end_host[]')->textInput([]) ?>
-        </div>
-        <div class="form-group col-md-3">
-            <?= $form->field($bandwidthCircuitServiceModel, 'z_end_ip[]')->textInput([]) ?>
+            <?= Html::Button('Search', ['class' => 'btn btn-primary', 'id' => 'searchbpacircuitreport']); ?>
         </div>
     </div>
-    <div class="form-group col-md-6"></div>
-    <div class="form-group col-md-2"><a id="addfields" style="margin-top:10px;cursor:pointer;">Add Circuit</a></div>
-    <div class="form-group col-md-2">
 
-        <?= Html::Button('Search', ['class' => 'btn btn-primary', 'id' => 'searchbpacircuitreport']); ?>
-    </div>
     <?php ActiveForm::end(); ?>
 
     <div class="bpa-usecase-circuit-data" id="bpacircuitreportdata">
